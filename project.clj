878fae991a52c9279336be2010258381bcd50bd0
Steven Obua <steven.obua@googlemail.com>
2012-12-28 18:13:57 +0000

    version 0.1.0

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject phlegmaticprogrammer/clojure_util "0.1.0"
+  :description "Various utility functions for Clojure."
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dev-dependencies [[lein-autodoc "0.9.0"]]
+  :test-path "src"
+  :aot :all
+  :plugins [[lein-swank "1.4.4"]])
---
+++ src/phlegmaticprogrammer/clojure_util/core.clj
@@ -0,0 +1,8 @@
+(ns phlegmaticprogrammer.clojure_util.core)
+
+(defn runtime-error [s] (throw (RuntimeException. s)))
+
+(defn reduce-indexed [f v coll]
+   (let [g (fn [[u index] c] [(f u index c) (+ index 1)])]
+     (first (reduce g [v 0] coll))))
+ 
